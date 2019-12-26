16bb45b166fd1e39bbb78f4e486b6d1d03b27be3
bhumigarach <59246093+bhumigarach@users.noreply.github.com>
2019-12-26 13:19:01 +0530

    2nd commit

---
+++ LinqwithAdo/WebForm1.aspx.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinqwithAdo
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            IEnumerable<Student> result = Student.GetAllStudent();
+            GridView1.DataSource = result;
+            GridView1.DataBind();
+        }
+    }
+}
